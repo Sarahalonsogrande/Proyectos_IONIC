629121b5ce2df4409afc891c06382ec14a893a7c
Sarah Alonso Grande <sarahalonsogrande@gmail.com>
2025-01-23 13:29:36 +0100

    Actualizo los ejercicios

--- movies-medium/src/app/components/ficha-pelicula/ficha-pelicula.component.ts
+++ movies-medium/src/app/components/ficha-pelicula/ficha-pelicula.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-ficha-pelicula',
   templateUrl: './ficha-pelicula.component.html',
   styleUrls: ['./ficha-pelicula.component.scss'],
+  imports: [IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle],
 })
 export class FichaPeliculaComponent  implements OnInit {
 
--- movies-medium/src/app/home/home.page.ts
+++ movies-medium/src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FichaPeliculaComponent } from "../components/ficha-pelicula/ficha-pelicula.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, FichaPeliculaComponent],
 })
 export class HomePage {
   constructor() {}
